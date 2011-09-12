4a2e6365e08bb813ab26f37af7a94c6c00f1536a
Your Name <your@email.com>
2011-09-12 17:50:18 -0500

    Pushing new changes

--- source/nothinbutdotnetprep/utility/filtering/Where.cs
+++ source/nothinbutdotnetprep/utility/filtering/Where.cs
@@ -32,5 +32,25 @@ namespace nothinbutdotnetprep.utility.filtering
         {
             return new AnonymousMatch<ItemToMatch>(x => property_accessor(x).CompareTo(value) > 0);
         }
+        public IMatchAn<ItemToMatch> less_than(PropertyType value)
+        {
+          return new AnonymousMatch<ItemToMatch>(x => property_accessor(x).CompareTo(value) < 0);
+        }
+        public IMatchAn<ItemToMatch> equal_to(PropertyType value)
+        {
+          return new AnonymousMatch<ItemToMatch>(x => property_accessor(x).CompareTo(value) == 0);
+        }
+        public IMatchAn<ItemToMatch> between(PropertyType valueStart, PropertyType valueEnd)
+        {
+          return new AnonymousMatch<ItemToMatch>(x =>
+                                                 (
+                                                   ((property_accessor(x).CompareTo(valueStart) > 0) ||
+                                                    (property_accessor(x).CompareTo(valueStart) == 0))
+                                                   &&
+                                                   ((property_accessor(x).CompareTo(valueEnd) < 0) ||
+                                                    (property_accessor(x).CompareTo(valueEnd) == 0))
+                                                 )
+            );
+        }
     }
 }
